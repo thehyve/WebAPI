e49ed0dfe5de119ed17f73f57111d1a0eaec1e27
Anthony Sena <asena5@its.jnj.com>
2017-12-13 08:53:40 -0500

    More patches

--- src/main/resources/resources/panacea/sql/runPanaceaStudy.sql
+++ src/main/resources/resources/panacea/sql/runPanaceaStudy.sql
@@ -8,147 +8,14 @@
 
 @tempTableCreationOracle
 
+DELETE FROM @pnc_ptsq_ct where job_execution_id = @jobExecId and study_id = @studyId; 
+
 @insertFromDrugEra
 @insertFromProcedure
 
--- sql server compatibility changes: no use of rownum, %%physloc%% for rowid (feed in with code for different dialect)
---MERGE INTO #_pnc_ptsq_ct ptsq
---USING
---(SELECT rank() OVER (PARTITION BY person_id
---  ORDER BY person_id, idx_start_date, idx_end_date, ROWNUM) real_tx_seq,
---  rowid AS the_rowid
---  FROM #_pnc_ptsq_ct 
---) ptsq1
---ON
---(
---     ptsq.rowid = ptsq1.the_rowid
---)
---WHEN MATCHED THEN UPDATE SET ptsq.tx_seq = ptsq1.real_tx_seq;
-
--- OHDSI-75: refactor merge here. ahh...
---MERGE INTO @pnc_ptsq_ct ptsq
---USING
---(SELECT rank() OVER (PARTITION BY person_id
---  ORDER BY person_id, idx_start_date, idx_end_date, concept_id) real_tx_seq,
---  @rowIdString AS the_rowid
---  FROM @pnc_ptsq_ct
---  WHERE job_execution_id = @jobExecId
---) ptsq1
---ON
---(
---     ptsq.@rowIdString = ptsq1.the_rowid
---)
---WHEN MATCHED THEN UPDATE SET ptsq.tx_seq = ptsq1.real_tx_seq;
---remove rowIdString
---with ptsq1 (real_tx_seq, the_rowid) as
---(SELECT rank() OVER (PARTITION BY person_id
---  ORDER BY person_id, idx_start_date, idx_end_date, concept_id) real_tx_seq,
---  @rowIdString AS the_rowid
---  FROM @pnc_ptsq_ct
---  WHERE job_execution_id = @jobExecId
---)
---update @pnc_ptsq_ct
---SET tx_seq = ptsq1.real_tx_seq
---from ptsq1
---where 
---@rowIdString = ptsq1.the_rowid;
---UPDATE  a
---SET     a.tx_seq = b.real_tx_seq
---FROM    @pnc_ptsq_ct a
---        INNER JOIN (SELECT rank() OVER (PARTITION BY person_id
---        ORDER BY person_id, idx_start_date, idx_end_date, concept_id) real_tx_seq,
---        *
---        FROM @pnc_ptsq_ct
---  WHERE  job_execution_id = @jobExecId) b
---on 
---  a.job_execution_id = b.job_execution_id
---  and a.study_id = b.study_id
---  and a.source_id = b.source_id
---  and a.person_id = b.person_id
---  and a.concept_id = b.concept_id
---  and a.concept_name = b.concept_name
---  and a.idx_start_date = b.idx_start_date
---  and a.idx_end_date = b.idx_end_date
---  and a.duration_days = b.duration_days;
---refactor again, postgres cannot do alias from "from" clause for update (apparently sqlrender does not support this either)
-UPDATE  @pnc_ptsq_ct
-SET     tx_seq = b.real_tx_seq
-FROM
-        (SELECT rank() OVER (PARTITION BY person_id
-        ORDER BY person_id, idx_start_date, idx_end_date, concept_id) real_tx_seq,
-        *
-        FROM @pnc_ptsq_ct
-  WHERE  job_execution_id = @jobExecId) as b
-where 
-  @pnc_ptsq_ct.job_execution_id = b.job_execution_id
-  and @pnc_ptsq_ct.study_id = b.study_id
-  and @pnc_ptsq_ct.source_id = b.source_id
-  and @pnc_ptsq_ct.person_id = b.person_id
-  and @pnc_ptsq_ct.concept_id = b.concept_id
-  and @pnc_ptsq_ct.concept_name = b.concept_name
-  and @pnc_ptsq_ct.idx_start_date = b.idx_start_date
-  and @pnc_ptsq_ct.idx_end_date = b.idx_end_date
-  and @pnc_ptsq_ct.duration_days = b.duration_days;
-
---IF OBJECT_ID('tempdb..#_pnc_sngl_cmb', 'U') IS NOT NULL
---  DROP TABLE #_pnc_sngl_cmb;
-
---CREATE TABLE #_pnc_sngl_cmb
---(
---  tx_stg_cmb_id INT,
---  concept_id INT,
---  concept_name VARCHAR(255)
---);
-
---get single concept combo
---insert into #_pnc_sngl_cmb (tx_stg_cmb_id, concept_id, concept_name)
---(select comb.pnc_tx_stg_cmb_id, combmap.concept_id, combmap.concept_name from @results_schema.pnc_tx_stage_combination comb
---join @results_schema.pnc_tx_stage_combination_map combMap 
---on combmap.pnc_tx_stg_cmb_id = comb.pnc_tx_stg_cmb_id
---join 
---(select comb.pnc_tx_stg_cmb_id, count(*) from @results_schema.pnc_tx_stage_combination comb
---join @results_schema.pnc_tx_stage_combination_map combMap 
---on combmap.pnc_tx_stg_cmb_id = comb.pnc_tx_stg_cmb_id
---where comb.study_id = @studyId
---group by comb.pnc_tx_stg_cmb_id
---having count(*) = 1) multiple_ids_combo
---on multiple_ids_combo.pnc_tx_stg_cmb_id = comb.pnc_tx_stg_cmb_id
---);
-
-
---MERGE INTO @results_schema.pnc_tx_stage_combination_map combo
---USING
---  (SELECT DISTINCT ptsq.concept_id concept_id, ptsq.concept_name concept_name FROM #_pnc_ptsq_ct ptsq
---    WHERE ptsq.concept_id NOT IN 
---      (select exist_combo.concept_id from 
---        (SELECT comb_map.concept_id concept_id FROM @results_schema.pnc_tx_stage_combination comb
---        JOIN @results_schema.pnc_tx_stage_combination_map comb_map
---        ON comb_map.pnc_tx_stg_cmb_id = comb.pnc_tx_stg_cmb_id
---        WHERE comb.study_id = @studyId) exist_combo
---      )
---  ) adding_concept
---  ON
---  (
---    1 = 0
---  )
---WHEN NOT MATCHED THEN INSERT (PNC_TX_STG_CMB_MP_ID, PNC_TX_STG_CMB_ID, CONCEPT_ID, CONCEPT_NAME)
---VALUES (@results_schema.seq_pnc_tx_stg_cmb_mp.NEXTVAL, @results_schema.seq_pnc_tx_stg_cmb.NEXTVAL, adding_concept.concept_id, adding_concept.concept_name);
-
 @insertIntoComboMapString
 
---move into specific db script with @insertIntoComboMapString
---MERGE INTO @results_schema.pnc_tx_stage_combination comb
---USING
---  (
---    SELECT combo_map.pnc_tx_stg_cmb_id pnc_tx_stg_cmb_id FROM @results_schema.pnc_tx_stage_combination_map combo_map
---  ) adding_combo
---  ON
---  (
---    comb.pnc_tx_stg_cmb_id = adding_combo.pnc_tx_stg_cmb_id
---  )
---WHEN NOT MATCHED THEN INSERT (PNC_TX_STG_CMB_ID,STUDY_ID)
---VALUES (adding_combo.pnc_tx_stg_cmb_id, @studyId);
-
+DELETE FROM @pnc_ptstg_ct where job_execution_id = @jobExecId and study_id = @studyId; 
 
 -- insert from #_pnc_ptsq_ct ptsq into #_pnc_ptstg_ct (remove same patient/same drug small time window inside large time window. EX: 1/2/2015 ~ 1/31/2015 inside 1/1/2015 ~ 3/1/2015)
 -- use single concept combo and avoid duplicate combo for the same concept if there's multiple single combo for same concept by min() value 
@@ -210,8 +77,9 @@ where not exists
         and duration_days = filteredPTSQ.duration_days
         and tx_seq = filteredPTSQ.tx_seq
 )
-order by person_id, idx_start_date, idx_end_date;
+--order by person_id, idx_start_date, idx_end_date;
 
+/*
 -- take care of expanded time window for same patient/same drug. 
 -- EX: 2/1/2015 ~ 4/1/2015 ptstg2, 1/1/2015 ~ 3/1/2015 ptstg1. Update ptstg1 with later end date and delete ptstg2   
 --OHDSI-75 refactor merge...
@@ -339,3 +207,4 @@ where exists
 
 --TRUNCATE TABLE #_pnc_tmp_cmb_sq_ct;
 --DROP TABLE #_pnc_tmp_cmb_sq_ct;
+*/
